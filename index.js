c10348ad2d23bbff384d30756b76906a95a5d70e
MukterHossain <mukterhossain3075@gmail.com>
2024-06-08 18:06:51 +0600

    added verify Token , verify admin

--- index.js
+++ index.js
@@ -49,13 +49,53 @@ app.post('/jwt', async(req, res) =>{
   res.send({token})
 })
 
+// middleware/ verify Token 
+const verifyToken = async(req, res, next) => {
+  console.log('inside verify token', req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+  
+}
+
+// use verify admin after verifyToken
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+  next()
+}
 
 //user related api
-app.get('/users', async(req, res) =>{
-  console.log(req.headers)
+app.get('/users', verifyToken, verifyAdmin, async(req, res) =>{
   const result = await userCollection.find().toArray();
-  console.log(result)
   res.send(result)
+})
+
+app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+  const email = req.params.email;
+  if(email !== req.decoded.email){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  const query = {email: email};
+  const user = await userCollection.findOne(query);
+  let admin = false;
+  if(user){
+    admin = user?.role === 'admin';
+  }
+  res.send({admin})
 })
 
 app.post('/users', async(req, res) =>{
